5cf9be56660d5b72ba2a002426e293d0b9e70bc3
Jaidi <jaidi@amazon.co.uk>
2019-06-10 16:17:37 +0100

    Logic to configure and set up bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 import click
 
 session = boto3.Session(profile_name='alexa-info-isengard')
@@ -21,8 +22,46 @@ def list_buckets():
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
+    """List objects in an S3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
+
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    """Create and configure S3 bucket."""
+    s3_bucket = None
+
+    try:
+        s3_bucket = s3.create_bucket(
+            Bucket=bucket,
+            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise error
+
+    policy = """
+    {
+      "Version": "2012-10-17",
+      "Statement": [{
+          "Sid": "GetS3Object",
+          "Action": ["s3:GetObject"],
+          "Effect": "Allow",
+          "Resource": "arn:aws:s3:::%s/*",
+          "Principal": {"AWS": ["arn:aws:iam::718896461338:user/jaidi"]}
+        }]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    return
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Webotron currently has the following features:
 
 - List bucket
 - List contents of a bucket
+- Set up and configure S3 bucket
