6c6f636e43214f5d39244328d8b5cb13113aba0e
Jaidi <jaidi@amazon.co.uk>
2019-06-13 17:32:48 +0100

    Sync based on ETag

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -4,17 +4,27 @@
 
 import mimetypes
 from pathlib import Path
+from hashlib import md5
+from functools import reduce
 
+import boto3
 from botocore.exceptions import ClientError
 
 
 class BucketManager:
     """Manage an S3 bucket."""
 
+    CHUNK_SIZE = 8388608
+
     def __init__(self, session):
         """Create a BucketManager object."""
         self.session = session
         self.s3 = self.session.resource('s3')
+        self.transfer_config = boto3.s3.transfer.TransferConfig(
+            multipart_chunksize=self.CHUNK_SIZE,
+            multipart_threshold=self.CHUNK_SIZE
+        )
+        self.manifest = {}
 
     def all_buckets(self):
         """Get an iterator for all buckets."""
@@ -59,22 +69,60 @@ class BucketManager:
         pol = bucket.Policy()
         pol.put(Policy=policy)
 
+    def load_manifest(self, bucket):
+        """Load manifest for caching purposes."""
+        paginator = self.s3.meta.client.get_paginator('list_objects_v2')
+        for page in paginator.paginate(Bucket=bucket.name):
+            for obj in page.get('Contents', []):
+                self.manifest[obj['Key']] = obj['ETag']
+
     @staticmethod
-    def upload_file(bucket, path, key):
+    def hash_data(data):
+        """Generate md5 hash for ETag."""
+        hash = md5()
+        hash.update(data)
+
+        return hash
+
+    def gen_etag(self, path):
+        """Generate a file ETag."""
+        hashes = []
+
+        data = Path(path).read_bytes()
+        hashes.append(self.hash_data(data))
+
+        if not hashes:
+            return
+        elif len(hashes) == 1:
+            return '"{}"'.format(hashes[0].hexdigest())
+        else:
+            digests = (h.digest() for h in hashes)
+            hash = self.hash_data(reduce(lambda x, y: x + y, digests))
+            return '"{}-{}"'.format(hash.hexdigest(), len(hashes))
+
+    def upload_file(self, bucket, path, key):
         """Upload file to S3 bucket."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
 
-        bucket.upload_file(
+        etag = self.gen_etag(path)
+
+        if self.manifest.get(key, '') == etag:
+            print("Skipping {}, etags match".format(key))
+            return
+
+        return bucket.upload_file(
             path,
             key,
             ExtraArgs={
                 'ContentType': content_type
-            }
+            },
+            Config=self.transfer_config
         )
 
     def sync_bucket(self, pathname, bucket_name):
         """Sync S3 bucket."""
         bucket = self.s3.Bucket(bucket_name)
+        self.load_manifest(bucket)
 
         root = Path(pathname).expanduser().resolve()
 
